d720def916fdc772589c9fe2c29cf3d69984508e
Vitor Leal <vitinho2201@gmail.com>
2018-10-15 12:09:10 -0300

    Adicionando picker e text
    Signed-off-by: Vitor Leal <vitinho2201@gmail.com>

--- lista05/App.js
+++ lista05/App.js
@@ -4,13 +4,54 @@ import {Button} from 'native-base';
 import DateTimePicker from 'react-native-modal-datetime-picker';
 
 export default class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      isVisible: false,
+      newTaskName:"",
+      newTaskTime:{},
+      taskList:[]
+    }
+  }
+
+  _showDateTimePicker = () => this.setState({ isVisible: true });
+
+  _hideDateTimePicker = () => this.setState({ isVisible: false });
+
+  _handleDatePicked = (date) => {
+    console.log(date);
+    this.setState({newTaskTime: date})
+    this._hideDateTimePicker();
+    this.addToList();
+  };
+
+  addToList(){
+    task = {
+      name: this.state.newTaskName,
+      date: this.state.newTaskTime
+    }
+    //this.setState({taskList: taskList.push(task)})
+    console.log(task)
+  }
+
   render() {
     return (
       <View style = {styles.view}>
         <Text>Welcome to React Native!</Text>
-        <Button rounded onPress = {() => {}} style = {styles.button}>
-          <Text> Adicionar nova tarefa </Text>
+        <TextInput
+          placeholder = "Nova tarefa"
+          style = {styles.input}
+          onChangeText={(newTaskName) => (this.setState({newTaskName}))}
+        />
+        <Button rounded onPress = {() => {this._showDateTimePicker()}} style = {styles.button}>
+          <Text style={{color:"#FFF"}}> Adicionar nova tarefa </Text>
         </Button>
+        <DateTimePicker
+          isVisible={this.state.isVisible}
+          onCancel = {this._hideDateTimePicker}
+          onConfirm = {this._handleDatePicked}
+          mode = 'datetime'
+        />
       </View>
     );
   }
@@ -18,7 +59,7 @@ export default class App extends Component{
 
 const styles = {
   button:{
-    color: 'red',
+    backgroundColor: 'red',
     height: 30,
     width: 150
   },
@@ -27,5 +68,8 @@ const styles = {
     flexDirection: 'column',
     justifyContent: 'center',
     padding: 20
+  },
+  input:{
+    borderColor:'red'
   }
 }
---
+++ lista05/newTask.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import {TextInput, Text, View} from 'react-native';
+import {Button} from 'native-base';
+
+export default class NewTask extends Component{
+    render(){
+        return(
+            <View></View>
+        );
+    }
+}
